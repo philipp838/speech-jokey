71d1ebd676e5589166a9deb8b98d0bca21052039
philipps <sa22b011@technikum-wien.at>
2025-02-25 16:58:05 +0100

    Added Coqui API to list

--- src/api/api_factory.py
+++ src/api/api_factory.py
@@ -14,7 +14,7 @@ class ApiFactory:
     """
     _instance = None
     apis = {}
-    api_names = ["GttsAPI","ElevenLabsAPI","OfaiAPI","OpenAIAPI","AmazonPollyAPI","MSAzureAPI"]
+    api_names = ["GttsAPI","ElevenLabsAPI","OfaiAPI","OpenAIAPI","AmazonPollyAPI","MSAzureAPI","CoquiAPI"]
     active_api_name = api_names[0]
 
     def __new__(cls, *args, **kwargs):
